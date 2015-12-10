09db47247ba82e523efd3bd58266558dec3a6d11
Poorvak <poorvak.kapoor@gmail.com>
2015-12-11 01:39:50 +0530

    changes in structure

--- go_appengine/run_server.py
+++ go_appengine/run_server.py
@@ -3,11 +3,17 @@ import os
 
 google_app_server_command = 'python dev_appserver.py --port={port_number} code_src'
 def run_server(port_number=None, datastore_file_name=None):
+	'''
+	run_server is an aggregator function method for running server
+
+	:port_number: port number for running server on
+	:datastore_file_name: for storing runtime data 
+	'''
 	if not port_number:
 		port_number = 8080
 	#datastore_path = file(datastore_path, 'r+') if os.path.exists(datastore_path) else file('tmp/app_datastore', 'w')
-
-	subprocess.call(google_app_server_command.format(port_number=port_number), shell=True)
+	final_command = google_app_server_command.format(port_number=port_number)
+	subprocess.call(final_command, shell=True)
 
 if __name__ == '__main__':
-	run_server(port_number=8080)
+	run_server()
